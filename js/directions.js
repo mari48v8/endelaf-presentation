bde03b024822bb056f285cb28cb0fdc2d5d6d81a
Marie Dyrmose <mari48v8@stud.kea.dk>
2019-01-13 12:19:17 +0100

    update

--- js/directions.js
+++ js/directions.js
@@ -1,10 +1,10 @@
 'use strict'
 
-//show form/ newuser.html when btnSignUp is clicked
+/* //show form/ newuser.html when btnSignUp is clicked
 btnSignUp.addEventListener('click', e => {
     document.location.href = 'newuser.html';
 
-})
+}) */
 
 //show loginForm.html when btnLogin is clicked
 btnLogin.addEventListener('click', e => {
@@ -17,4 +17,3 @@ btnLogout.addEventListener('click', e => {
     document.location.href = '../index.html';
 
 })
-
